afde873225ae7c7574972a9e27b50af248b7a2e3
Majukas <rytistamosiunas@yahoo.com>
2019-07-03 13:51:58 +0300

    Pakeitimai padaryti

--- src/main/java/lt/bta/Main.java
+++ src/main/java/lt/bta/Main.java
@@ -2,6 +2,7 @@ package lt.bta;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hye");
         System.out.println("Hello world");
         PersonKt.printHello();
     }
